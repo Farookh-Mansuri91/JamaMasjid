1ef34bbf2fc462fb58e5cd0a0c9764a4742055f9
Farookh-Mansuri91 <farookh.mansuri91@gmail.com>
2025-11-03 00:07:18 +0530

    income url update

--- src/app/Services/MasjidIncomeExpenseService/apiService.js
+++ src/app/Services/MasjidIncomeExpenseService/apiService.js
@@ -2,7 +2,7 @@
 
 export const fetchMasjidIncomeExpensesData = async () => {
     try {
-      const response = await fetch(`https://api.noorimasjidghanghori.com/api/GetIncomeExpenseData`);
+      const response = await fetch(`https://api.noorimasjidghanghori.com/GetIncomeExpenseData`);
       if (!response.ok) {
         throw new Error('Failed to fetch data from the server.');
       }
