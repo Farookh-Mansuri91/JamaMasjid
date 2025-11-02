c229d4b31ea60f528318dc0767919e29d3936c27
Farookh-Mansuri91 <farookh.mansuri91@gmail.com>
2025-11-02 21:49:01 +0530

    masjid commette service url update

--- src/app/Services/MasjidCommitteeService/apiService.js
+++ src/app/Services/MasjidCommitteeService/apiService.js
@@ -1,7 +1,7 @@
 // apiService.js
 export const fetchMasjidCommitteeData = async () => {
     try {
-      const response = await fetch('https://localhost:7140/api/MasjidCommittee');
+      const response = await fetch('https://api.noorimasjidghanghori.com/api/MasjidCommittee');
       if (!response.ok) {
         throw new Error('Failed to fetch data from the server.');
       }
