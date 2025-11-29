c721a18d676542d1ed06e11a79987f8012367cdc
Farookh-Mansuri91 <farookh.mansuri91@gmail.com>
2025-11-29 11:39:32 +0530

    auth helper update

--- src/app/Utils/authHelpers.js
+++ src/app/Utils/authHelpers.js
@@ -12,13 +12,30 @@ export const getUserRole = () => {
   }
   return null; // Default to null in non-browser environments
 };
+export const getUserId = () => {
+  if (typeof window !== "undefined") {
+    return localStorage.getItem("userId");
+  }
+  return null; // Default to null in non-browser environments
+};
+export const getMemberId = () => {
+  if (typeof window !== "undefined") {
+    return localStorage.getItem("memberId");
+  }
+  return null; // Default to null in non-browser environments
+};
 export const getUserData = () => {
   const user = localStorage.getItem('userName'); 
   const role = localStorage.getItem('role');
+  const userId = localStorage.getItem('userId');
+  const memberId = localStorage.getItem('memberId');
+
   // Create an object with the user's name and role
   const userData = {
     name: user,
-    role: role
+    role: role,
+    userId: userId,
+    memberId: memberId
   }
   return userData ? userData : null; // Return the user object (which should contain name, role, etc.)
 }
@@ -26,5 +43,7 @@ export const logout = () => {
   localStorage.removeItem("token");
   localStorage.removeItem("role");
   localStorage.removeItem("userName");
+  localStorage.removeItem("userId");
+  localStorage.removeItem("memberId");
   window.location.href = "/Login";
 };  
